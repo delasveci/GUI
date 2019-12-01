07110b027c53d8c0f857e8a317b58dd859d57b3f
delasveci <58400637+delasveci@users.noreply.github.com>
2019-12-01 22:11:03 +0100

    first version of sample.py

--- sample/sample.py
+++ sample/sample.py
@@ -1,1 +1,30 @@
+print "hi user"
+print "we are going to calculate your gross salary per week"
 
+h_week = raw_input('How many hours you work per week: ')
+try:
+    float(h_week)
+except:
+    print 'You have enter an no acceptable value of hours'
+    quit()
+
+rate = raw_input('How is the rate per hour: ')
+try:
+    float(rate)
+except:
+    print 'You have enter an no acceptable value of rate'
+    quit()
+
+
+def print_puta():
+    print 'imprimiendo a tu madre'
+
+
+if int(h_week) > 40:
+    gross = float(h_week) * float(rate) + 5 * (float(h_week) - 40)
+else:
+    gross = float(h_week) * float(rate)
+
+print 'Your gross salary is ', gross, ' dolars/week'
+
+print_puta()
